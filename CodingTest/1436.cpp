7c8ef0dac6f1a823af445122d3020263bff1c573
황현민 <hwanghyunmin0650@gmail.com>
2023-10-30 11:14:57 +0900

    백준 1436

---
+++ CodingTest/1436.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int i = 0;
+//
+//	int result = 0;
+//	while (true) {
+//		result++;
+//
+//		string str = to_string(result);
+//		if (str.find("666") != string::npos) i++;
+//
+//		if (i == n) {
+//			cout << str << endl;
+//			break;
+//		}
+//	}
+//
+//	return 0;
+//}
